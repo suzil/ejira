190410022457265a7c934396a2195260e8f36546
Susannah Klaneček <suzil@protonmail.ch>
2019-07-25 21:43:57 +0300

    Use API Token auth

--- jiralib2.el
+++ jiralib2.el
@@ -57,36 +57,13 @@
   :type 'string
   :group 'jiralib2)
 
-(defvar jiralib2-user-login-name nil
-  "The name of the user logged into JIRA.
-This is maintained by `jiralib2-login'.")
+;; (defvar jiralib2-user-login-name nil
+;;   "The name of the user logged into JIRA.
+;; This is maintained by `jiralib2-login'.")
 
-(defvar *JIRA-SESSION* nil
-  "Contains the cookie of the active JIRA session.")
-
-(defun jiralib2-session-login (&optional username password)
-  "Login to JIRA with USERNAME and PASSWORD. Save cookie in *JIRA-SESSION*."
-  (interactive)
-  (setq *JIRA-SESSION*
-        (let* ((username (or username
-                             jiralib2-user-login-name
-                             (read-string "Username: ")))
-               (password (or password
-                             (read-passwd (format "Password for user %s: "
-                                                  username))))
-               (reply-data (request (concat jiralib2-url "/rest/auth/1/session")
-                                    :type "POST"
-                                    :headers `(("Content-Type" . "application/json"))
-                                    :parser 'json-read
-                                    :sync t
-                                    :data (json-encode `((username . ,username)
-                                                         (password . ,password)))))
-               (status-code (request-response-status-code reply-data))
-               (auth-info (cdar (jiralib2--verify-status reply-data)))
-               (session-token (format "%s=%s"
-                                      (cdr (assoc 'name auth-info))
-                                      (cdr (assoc 'value auth-info)))))
-          session-token)))
+;; (defvar jiralib2-api-token nil
+;;   "The name of the user logged into JIRA.
+;; This is maintained by `jiralib2-login'.")
 
 (defun jiralib2--verify-status (response)
   "Check status code of RESPONSE, return data or throw an error."
@@ -114,76 +91,77 @@ This is maintained by `jiralib2-login'.")
           ;; status codes 200 - 399 should be ok.
           (t (request-response-data response)))))
 
+(defun jiralib2--api-call (path args)
+  "Do a call to PATH with ARGS to the API."
+  (when (not (boundp 'jiralib2-user-login-name))
+    (user-error "jiralib2-user-login-name must be defined"))
 
-(defun jiralib2-get-user-info ()
-  "Fetch information on currently logged in user."
-  (jiralib2-session-call "/rest/api/2/myself"))
-
-(defun jiralib2--session-call (path args)
-  "Do a call to PATH with ARGS using current session.
-Does not check for session validity."
+  (when (not (boundp 'jiralib2-api-token))
+    (user-error "jiralib2-api-token must be defined"))
+  
   (apply #'request (concat jiralib2-url path)
          :headers `(("Content-Type" . "application/json")
-                    ("cookie" . ,*JIRA-SESSION*))
+                    ("Authorization" .
+		     ,(concat "Basic "
+			      (base64-encode-string
+			       (concat
+				jiralib2-user-login-name
+				":"
+				jiralib2-api-token)))))
          :sync t
          :parser 'json-read
          args))
 
-(defun jiralib2-session-call (path &rest args)
-  "Do a call to PATH with ARGS using current session.
-If no session exists, or it has expired, login first."
-  (unless *JIRA-SESSION*
-    (jiralib2-session-login))
-
-  (let ((response (jiralib2--session-call path args)))
+(defun jiralib2-api-call (path &rest args)
+  "Call the Jira REST API and make sure the response is OK."
+  (let ((response (jiralib2--api-call path args)))
 
     (unless (request-response-status-code response)
       (user-error "Call failed: Could not reach the server"))
 
-    ;; The session has probably expired. Login and try again.
-    (when (= (request-response-status-code response) 401)
-      (message "Session expired, retrying...")
-      (jiralib2-session-login)
-      (setq response (jiralib2--session-call path args)))
     (jiralib2--verify-status response)))
+
+(defun jiralib2-get-user-info ()
+  "Fetch information on currently logged in user."
+  (jiralib2-api-call "/rest/api/2/myself"))
 
 (defun jiralib2-get-issue (issue-key)
   "Get the issue with key ISSUE-KEY."
-  (jiralib2-session-call (format "/rest/api/2/issue/%s" issue-key)))
+  (jiralib2-api-call (format "/rest/api/2/issue/%s" issue-key)))
 
 (defun jiralib2-add-comment (issue-key body)
   "Add comment to issue ISSUE-KEY with contents BODY."
-  (jiralib2-session-call (format "/rest/api/2/issue/%s/comment" issue-key)
-                         :type "POST"
-                         :data (json-encode `((body . ,body)))))
+  (jiralib2-api-call (format "/rest/api/2/issue/%s/comment" issue-key)
+                     :type "POST"
+                     :data (json-encode `((body . ,body)))))
 
 
 (defun jiralib2-delete-comment (issue-key comment-id)
   "Remove comment COMMENT-ID from issue ISSUE-KEY."
-  (jiralib2-session-call (format "/rest/api/2/issue/%s/comment/%s"
-                                 issue-key comment-id)
-                         :type "DELETE"))
+  (jiralib2-api-call (format "/rest/api/2/issue/%s/comment/%s"
+                             issue-key comment-id)
+                     :type "DELETE"))
 
 (defun jiralib2-edit-comment (issue-key comment-id body)
   "Update comment COMMENT-ID from issue ISSUE-KEY with body BODY."
-  (jiralib2-session-call (format "/rest/api/2/issue/%s/comment/%s"
-                                 issue-key comment-id)
-                         :type "PUT"
-                         :data (json-encode `((body . ,body)))))
+  (jiralib2-api-call (format "/rest/api/2/issue/%s/comment/%s"
+                             issue-key comment-id)
+                     :type "PUT"
+                     :data (json-encode `((body . ,body)))))
 
 (defvar *jiralib2-users-cache* nil)
 (defun jiralib2-get-users (project-key)
   "Return assignable users information given the PROJECT-KEY."
   (or *jiralib2-users-cache*
-      (jiralib2-session-call
+      (jiralib2-api-call
        (format "/rest/api/2/user/assignable/search?project=%s&maxResults=10000"
                project-key))))
 
 (defun jiralib2-assign-issue (issue-key username)
   "Assign issue with ISSUE-KEY to USERNAME."
-  (jiralib2-session-call (format "/rest/api/2/issue/%s/assignee" issue-key)
-                         :type "PUT"
-                         :data (json-encode `((name . ,username)))))
+  (jiralib2-api-call (format "/rest/api/2/issue/%s/assignee" issue-key)
+                     :type "PUT"
+                     :data (json-encode `((name . ,username)))))
 
 (defun jiralib2-do-jql-search (jql &optional limit)
   "Run a JQL query and return the list of issues that matched.
@@ -196,11 +174,11 @@ query."
   (append
    (cdr
     (assoc 'issues
-           (jiralib2-session-call "/rest/api/2/search"
-                                  :type "POST"
-                                  :data (json-encode
-                                         `((jql . ,jql)
-                                           (maxResults . ,limit))))))
+           (jiralib2-api-call "/rest/api/2/search"
+                              :type "POST"
+                              :data (json-encode
+                                     `((jql . ,jql)
+                                       (maxResults . ,limit))))))
    nil))
 
 
@@ -211,61 +189,61 @@ The issues are returned as a list of ((name . <name>) (id . <id>)) alists."
    (lambda (trans)
      `(,(cdr (assoc 'id trans)) . ,(cdr (assoc 'name trans))))
    (cdadr
-    (jiralib2-session-call (format "/rest/api/2/issue/%s/transitions"
-                                   issue-key)))))
+    (jiralib2-api-call (format "/rest/api/2/issue/%s/transitions"
+                               issue-key)))))
 
 (defun jiralib2-do-action (issue-key action-id)
   "Move the issue ISSUE-KEY to another state with action ACTION-ID."
-  (jiralib2-session-call (format "/rest/api/2/issue/%s/transitions" issue-key)
-                         :type "POST"
-                         :data (json-encode `((transition . ((id . ,action-id)))))))
+  (jiralib2-api-call (format "/rest/api/2/issue/%s/transitions" issue-key)
+                     :type "POST"
+                     :data (json-encode `((transition . ((id . ,action-id)))))))
 
 
 (defun jiralib2-get-worklog (issue-key &optional only-mine)
   "Get worklogs of the issue ISSUE-KEY.
 With ONLY-MINE set to t, only return worklogs logged by me."
-  (jiralib2-session-call (format "/rest/api/2/issue/%s/worklog" issue-key)))
+  (jiralib2-api-call (format "/rest/api/2/issue/%s/worklog" issue-key)))
 
 (defun jiralib2-add-worklog (issue-key timestamp seconds message)
   "Add a worklog to issue ISSUE-KEY with message MESSAGE.
 Use TIMESTAMP as start time and SECONDS as amount of logged work in seconds."
-  (jiralib2-session-call (format "/rest/api/2/issue/%s/worklog" issue-key)
-                         :type "POST"
-                         :data (json-encode `((comment . ,message)
-                                              (started . ,timestamp)
-                                              (timeSpentSeconds . ,seconds)))))
+  (jiralib2-api-call (format "/rest/api/2/issue/%s/worklog" issue-key)
+                     :type "POST"
+                     :data (json-encode `((comment . ,message)
+                                          (started . ,timestamp)
+                                          (timeSpentSeconds . ,seconds)))))
 
 (defvar *jiralib2-projects-cache* nil)
 (defun jiralib2-get-projects ()
   "Get a list of all projects."
   (or *jiralib2-projects-cache*
       (setq *jiralib2-projects-cache*
-            (jiralib2-session-call "/rest/api/2/project"))))
+            (jiralib2-api-call "/rest/api/2/project"))))
 
 (defvar *jiralib2-issuetypes-cache* nil)
 (defun jiralib2-get-issuetypes ()
   "Get a list of all projects."
   (or *jiralib2-issuetypes-cache*
       (setq *jiralib2-issuetypes-cache*
-            (jiralib2-session-call "/rest/api/2/issuetype"))))
+            (jiralib2-api-call "/rest/api/2/issuetype"))))
 
 (defun jiralib2-create-issue (project-id summary description)
   "Create a new issue into project PROJECT-ID with SUMMARY and DESCRIPTION."
-  (jiralib2-session-call "/rest/api/2/issue/"
-                         :type "POST"
-                         :data (json-encode
-                                `((fields . ((project . ,project-id)
-                                             (summary . ,summary)
-                                             (description . ,description)))))))
+  (jiralib2-api-call "/rest/api/2/issue/"
+                     :type "POST"
+                     :data (json-encode
+                            `((fields . ((project . ,project-id)
+                                         (summary . ,summary)
+                                         (description . ,description)))))))
 
 (defun jiralib2-update-summary-description (issue-id summary description)
   "Change the summary and description of issue ISSUE-ID to SUMMARY and DESCRIPTION."
   (interactive)
-  (jiralib2-session-call (format "/rest/api/2/issue/%s" issue-id)
-                         :type "PUT"
-                         :data (json-encode
-                                `((fields . ((description . ,description)
-                                             (summary . ,summary)))))))
+  (jiralib2-api-call (format "/rest/api/2/issue/%s" issue-id)
+                     :type "PUT"
+                     :data (json-encode
+                            `((fields . ((description . ,description)
+                                         (summary . ,summary)))))))
 
 
 (provide 'jiralib2)
